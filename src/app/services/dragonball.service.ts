0d9b6ea22b8ea7d84500a37d37665fd857c0432f
Alejandro Ogando <aoseptien@gmail.com>
2025-12-10 23:34:12 -0500

    feat: refactor DragonballService to load characters from localStorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,13 +1,21 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+function loadFromLocalStorage(): Character[] {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
   // Estado centralizado
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegueta', power: 8000 },
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+  // Effect para guardar en localStorage cada vez que cambian los personajes
+  saveToLocalStorage = effect(() => {
+    // console.log(`Characters count: ${this.characters().length}`);
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   // Método que recibe los datos desde el componente
   addCharacter(name: string, power: number): boolean {
